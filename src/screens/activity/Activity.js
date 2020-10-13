a95d93ab2866c36c4d8d7fac812f7fcd9babc29d
kate <katok0709@gmail.com>
2020-10-13 17:52:25 +0300

    some layout for components made

--- src/screens/activity/Activity.js
+++ src/screens/activity/Activity.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import './Activity.css'
 
 export default function Activity() {
     return (
-        <div>
-            <h1>This is activity component!</h1>
-        </div>
+        <body>
+        <main>
+          <div>
+            Делать то-то так-то, столько-то <br />
+            Сложность: такая-то
+            <button onclick="location.href='/screens/get quest'">
+              Взять квест
+            </button>
+            <div>
+              <button onclick="location.href='/screens/add edit activity'">
+                Уточнить действие
+              </button>
+              <button onclick="location.href='/screens/activities'">Удалить</button>
+            </div>
+            Способствует в стремлениях:
+            <ul id="endeavors" onclick="if (event.target!=this)
+              location.href='/screens/endeavor'">
+      
+              <li>Sit dui fringilla, ligula vigoro</li>
+              <li>Metus spero sagittis orci</li>
+              <li>Cras perpetum habitasse ac vici faucibus curabitur morbi</li>
+            </ul>
+            <div>
+              <button onclick="location.href='/screens/endeavors'">
+                Связать существующее
+              </button>
+              <button onclick="location.href='/screens/add edit endeavor'">
+                Создать связанное
+              </button>
+            </div>
+          </div>
+        </main>
+      </body>
     )
 }
--- src/screens/add-edit-activity/AddEditActivity.js
+++ src/screens/add-edit-activity/AddEditActivity.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import './AddEditActivity.css'
 
 export default function AddEditActivity() {
     return (
-        <div>
-            <h1>This is AddEditActivity component!</h1>
-        </div>
+        <body>
+            <main>
+                <div>
+                    Какое действие нужно регулярно выполнять (или избегать) <br />
+      для приближения к каким-то из твоих стремлений? <br />
+                    <input type="text" /> <br />
+      Укажи количественную меру выполнения в день <br />
+      (сколько минут, повторений, кругов, раз...) <br />
+                        <input type="text" /> <br />
+      регулярность <button onclick="location.href='/screens/schemas'">
+                                ежедневно
+      </button> <br />
+      Оцени сложность серии из X выполнений?
+      <input type="number" min="1" max="10" /> <br />
+                                <button onclick="location.href='/screens/activities'">Запомнить действие</button>
+    </div>
+  </main>
+</body>
     )
 }
