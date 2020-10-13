40f1d80abb764f7f616100a32ac6575b238a6f88
unknown <xin90210@gmail.com>
2020-10-14 01:59:19 +0300

    change routing

--- src/RouterFile.js
+++ src/RouterFile.js
@@ -5,8 +5,8 @@ import {
     Route,
     Link
 } from 'react-router-dom'
-
-
+import Classcomponent from './Classcomponent'
+import Functioncomponent from './Functioncomponent'
 
 import Activities from './screens/activities/Activities'
 import Activity from './screens/activity/Activity'
@@ -27,6 +27,12 @@ export default class RouterFile extends Component {
                         </Route>
                         <Route path="/Add-edit-activity">
                             <AddEditActivity />
+                        </Route>
+                        <Route path="/Classcomponent">
+                            <Classcomponent />
+                        </Route>
+                        <Route path="/Functioncomponent">
+                            <Functioncomponent />
                         </Route>
                     </Switch>
                 </div>
