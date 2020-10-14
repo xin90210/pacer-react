55d5d9decf52600fedf8d11e90c6d889ca7c49d1
kate <katok0709@gmail.com>
2020-10-14 03:03:08 +0300

    activities changed

---
+++ src/screens/activities/Activities.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Activities.css'
+
+export default function Activities() {
+    return (
+        <div>
+            <button>Добавить Действие</button>
+            <div className="list">
+                <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, quis.</li>
+                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, non.</li>
+                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, non.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptas minus ab dicta voluptatem cupiditate mollitia iste reiciendis, assumenda consequatur.</li>
+                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, non.</li>
+                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, non.</li>
+                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, non.</li>
+                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum molestias dolorem voluptatem saepe beatae temporibus.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur nulla nihil unde distinctio ab atque, officia dolores hic iusto!</li>
+                    <li></li>
+                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, non.</li>
+                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, non.</li>
+                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, non.</li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+
+    )
+}
